39269981a60466c27f64ce559a1088feae5ce817
Brandon Presley <brandon@presley.nz>
2024-12-06 13:20:49 +1300

    Add image to buttons lesson

--- app/buttons.tsx
+++ app/buttons.tsx
@@ -1,9 +1,22 @@
-import { Button, View } from "react-native";
+import { Button, Image, View } from "react-native";
+import { styles, vibrate } from "./utils";
 
 export default function Buttons() {
-    return (
-        <View>
-            <Button title="Tap me" />
-        </View>
-    )
+  async function tapped() {
+    // TODO: Log  your name here
+    vibrate();
+  }
+
+  return (
+    <View>
+      {/* TODO: Change this to a different image */}
+      <Image
+        source={require("../assets/images/cat.png")}
+        style={styles.image}
+      />
+
+      {/* TODO: Make this button say your name */}
+      <Button title="Vibrate" onPress={tapped} />
+    </View>
+  );
 }
