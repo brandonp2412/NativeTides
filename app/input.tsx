ccbec0ba945c66b3b9a35834f49a65fdbb9f26ba
Brandon Presley <brandon@presley.nz>
2024-12-06 17:42:07 +1300

    Make notifications a part of input lesson

--- app/input.tsx
+++ app/input.tsx
@@ -1,10 +1,29 @@
-import { View, TextInput } from "react-native";
-import { styles } from "./utils";
+import { View, TextInput, Text } from "react-native";
+import { notify, styles } from "./utils";
+import { useState } from "react";
 
 export default function Input() {
+  const [name, setName] = useState("");
+
+  function sayName() {
+    notify("Your name is", name);
+  }
+
   return (
-    <View>
-      <TextInput style={styles.input} placeholder="Type your name here..." />
+    <View style={styles.center}>
+      <Text style={styles.title}>Input</Text>
+      <Text>To get input from the user, use TextInput</Text>
+
+      <TextInput
+        value={name}
+        onChangeText={setName}
+        onSubmitEditing={sayName}
+        style={styles.input}
+        placeholder="Type your name here..."
+      />
+
+      {/* TODO: Ask for the users age */}
+      {/* TODO: Ask for the users date of birth */}
     </View>
   );
 }
--- app/utils.ts
+++ app/utils.ts
@@ -1,15 +1,18 @@
 import { StyleSheet } from "react-native";
+import * as Notifications from "expo-notifications";
 import * as Haptics from "expo-haptics";
 
 export const styles = StyleSheet.create({
   title: {
     fontSize: 26,
-    marginBottom: 16,
+    textAlign: "center",
   },
   center: {
+    gap: 16,
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    margin: 8,
   },
   image: {
     height: 300,
@@ -33,10 +36,46 @@ export const styles = StyleSheet.create({
     shadowRadius: 5,
     elevation: 4,
   },
+  link: {
+    color: "#007AFF",
+    fontSize: 18,
+    fontWeight: "500",
+  },
 });
 
 export async function vibrate() {
   return Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error);
+}
+
+// Request permissions for notifications
+async function requestPermissionsAsync() {
+  const { status } = await Notifications.requestPermissionsAsync();
+  if (status !== "granted") {
+    alert("You need to enable permissions in settings");
+    return;
+  }
+}
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+// Schedule a notification
+export async function notify(title: string, body: string) {
+  await requestPermissionsAsync(); // Ensure permissions are granted
+
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title,
+      body,
+      priority: Notifications.AndroidNotificationPriority.HIGH, // Set the priority
+    },
+    trigger: null,
+  });
 }
 
 export default {};
